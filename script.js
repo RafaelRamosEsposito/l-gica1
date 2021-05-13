52993dac8e3243e2349ad5e8f4e0af7bea4a1688
RafaelRamosEsposito <rafaelramosesposito@gmail.com>
2021-05-13 11:59:09 -0300

    finalizei primeira atividade de logica

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Rafael"
+let idade = 36
+let ano = 2021 - 36
+console.log("Meu ano de nascimento é", ano)
+const souhumano = true
+let idademae = 61
+let idadepai = 66
+console.log(idademae + idadepai)
